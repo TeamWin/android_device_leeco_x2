e586564ef671fd9c986179bbe00ceb70b2dc7bee
AndroPlus <hosigumayuugi@gmail.com>
2016-09-22 22:21:21 +0900

    Fix mouse pointer issue

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -58,3 +58,4 @@ TW_INCLUDE_CRYPTO := true
 TARGET_RECOVERY_DEVICE_MODULES := libbinder libgui libui libEGL libGLES_trace libGLESv2 libprotobuf-cpp-lite libsync
 TW_RECOVERY_ADDITIONAL_RELINK_FILES := $(OUT)/system/lib64/libbinder.so $(OUT)/system/lib64/libgui.so $(OUT)/system/lib64/libui.so $(OUT)/system/lib64/libEGL.so $(OUT)/system/lib64/libGLES_trace.so $(OUT)/system/lib64/libGLESv2.so $(OUT)/system/lib64/libprotobuf-cpp-lite.so $(OUT)/system/lib64/libsync.so
 TW_NEW_ION_HEAP := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
