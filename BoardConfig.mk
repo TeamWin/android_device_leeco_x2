123e417f91a1009f621103fe1ea32c7e7e6ed538
LuK1337 <priv.luk@gmail.com>
2019-09-26 14:40:41 +0200

    x2: Override platform version to 16.1.0
    * That way we can have TWRP that decrypts userdata
      on more than just <= Android 9.
    
    Change-Id: I0d18187b13c2b5a1a9a5a592010b604c57543c03

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,6 +25,7 @@ TARGET_2ND_CPU_VARIANT := cortex-a53
 TARGET_CRYPTFS_HW_PATH := vendor/qcom/opensource/commonsys/cryptfs_hw
 TARGET_HW_DISK_ENCRYPTION := true
 PLATFORM_SECURITY_PATCH := 2099-12-31
+PLATFORM_VERSION := 16.1.0
 
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom 
